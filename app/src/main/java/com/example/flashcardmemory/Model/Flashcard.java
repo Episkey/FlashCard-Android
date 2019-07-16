d9934bfc202d06d2043f3b12121e92969a77dadc
Episkey <paul.beziat66@gmail.com>
2019-07-16 17:34:58 +0200

    models + volley singleton + manifest + gradle initialisation + package

--- app/src/main/java/com/example/flashcardmemory/Model/Flashcard.java
+++ app/src/main/java/com/example/flashcardmemory/Model/Flashcard.java
@@ -1,4 +1,68 @@
 package com.example.flashcardmemory.Model;
 
 public class Flashcard {
+
+    private Long idFlashcard;
+
+    private String name;
+
+    private String question;
+
+    private String answer;
+
+    private boolean learned;
+
+    private User user;
+
+    public Flashcard() {
+
+    }
+
+    public Long getIdFlashcard() {
+        return idFlashcard;
+    }
+
+    public void setIdFlashcard(Long idFlashcard) {
+        this.idFlashcard = idFlashcard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isLearned() {
+        return learned;
+    }
+
+    public void setLearned(boolean learned) {
+        this.learned = learned;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
