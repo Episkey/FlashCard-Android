87582d56859941948ba4f60d5d609e2b0087b215
Episkey <paul.beziat66@gmail.com>
2019-07-17 14:06:01 +0200

    SignIn & SignUp création

--- app/src/main/java/com/example/flashcardmemory/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/flashcardmemory/Activity/SignUpActivity.java
@@ -1,9 +1,24 @@
 package com.example.flashcardmemory.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Consumer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.flashcardmemory.Fragment.ListFlashcardFragment;
+import com.example.flashcardmemory.Model.User;
 import com.example.flashcardmemory.R;
+import com.example.flashcardmemory.Singleton.UserSingleton;
+import com.example.flashcardmemory.Singleton.VolleySingleton;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.Charset;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -11,5 +26,43 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        TextView alreadyUser = findViewById(R.id.tvGoSignIn);
+        alreadyUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+            }
+        });
+
+        final EditText etMail = findViewById(R.id.etMail);
+        final EditText etpassword = findViewById(R.id.etPassword);
+
+        Button btSingUp = findViewById(R.id.btSignUp);
+        btSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etMail.getText().toString().isEmpty()
+                        & !etpassword.getText().toString().isEmpty()) {
+                    User user = new User();
+                    user.setMail(etMail.getText().toString());
+                    HashCode hashCode = Hashing.sha256().hashString(etpassword.getText().toString(), Charset.defaultCharset());
+                    user.setPassword(hashCode.toString());
+                    VolleySingleton.getInstance(getApplicationContext()).createUser(user, new Consumer<User>() {
+                        @Override
+                        public void accept(User user) {
+                            UserSingleton.getInstance().setUser(user);
+                            startActivity(new Intent(SignUpActivity.this, ListFlashcardFragment.class));
+                        }
+                    });
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setTitle(getString(R.string.please_fill));
+                    builder.setPositiveButton(getString(R.string.ok), null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/flashcardmemory/Singleton/VolleySingleton.java
+++ app/src/main/java/com/example/flashcardmemory/Singleton/VolleySingleton.java
@@ -1,15 +1,33 @@
 package com.example.flashcardmemory.Singleton;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.v4.util.Consumer;
+import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.flashcardmemory.Model.Authentication;
+import com.example.flashcardmemory.Model.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VolleySingleton {
 
     public static final String ERROR_EMAIL = "ERROR_EMAIL";
-    public static final String ERROR_PASSWORD = "ERROR_PASSWORD";
-    private final static String REQUEST_URL = "http://id:8080/";
+    private final static String REQUEST_URL = "http://192.168.8.135:8080/";
     private static VolleySingleton instance;
     private static Context ctx;
     private RequestQueue requestQueue;
@@ -31,5 +49,83 @@ public class VolleySingleton {
             requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
         }
         return requestQueue;
+    }
+
+    public void createUser(final User user, final Consumer<User> listener) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+        String url = REQUEST_URL + "users";
+        final String requestBody = gson.toJson(user);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("VOLLEY_SUCCESS", response.toString());
+                User user = gson.fromJson(response.toString(), User.class);
+                listener.accept(user);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.accept(null);
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    return requestBody == null ? null : requestBody.getBytes(StandardCharsets.UTF_8);
+                }
+                return null;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public void getUserByEmail(User user, final Consumer<Authentication> listener) {
+        String url = REQUEST_URL + "users/search";
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+        final String requestBody = gson.toJson(user);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("VOLLEY_SUCCESS", response.toString());
+                Authentication authentication = gson.fromJson(response.toString(), Authentication.class);
+                listener.accept(authentication);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.accept(null);
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    return requestBody == null ? null : requestBody.getBytes(StandardCharsets.UTF_8);
+                }
+                return null;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
     }
 }
